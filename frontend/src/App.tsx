d81264b49624d92387f33d11abe7b7267205f419
Jaewall <jaedawaldram@gmail.com>
2023-03-31 23:28:17 -0600

    Putting it together

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,19 +1,41 @@
-import React from "react";
-import logo from "./logo.svg";
-import "./App.css";
-
-class Welcome extends React.Component {
-  render() {
-    return <h1>This website gives some rando numbers</h1>;
-  }
-}
+// import React, { useState } from 'react';
+// import logo from './logo.svg';
+import './App.css';
+import TopBanner from './Home';
 
 function App() {
   return (
-    <div>
-      <Welcome />
+    <div className="App">
+      <TopBanner />
     </div>
   );
 }
 
 export default App;
+
+// function GoButton(props: any) {
+//   return (
+//     <div>
+//       <button onClick={() => setCount(props.count + 1)}>Go</button>
+//       <br />
+//       <label>{props.count}</label>
+//     </div>
+//   );
+// }
+
+// function AmountofGo(props: any) {
+//   return <label>{props.count}</label>;
+// }
+
+// function App() {
+//   const [count, setCount] = useState(0);
+
+//   return (
+//     <div>
+//       <GoButton />
+//       <AmountofGo count={count} />
+//     </div>
+//   );
+// }
+
+// export default App;
---
+++ frontend/src/Home.tsx
@@ -0,0 +1,15 @@
+function TopBanner() {
+  return (
+    <>
+      <div className="row"></div>
+      <div>
+        <img src="./JoelHiltonHeadshot.jpg" alt="JoelHeadshot" />
+      </div>
+      <div className="col align-self-center ">
+        <h1>Welcome to my movie collection website</h1>
+      </div>
+    </>
+  );
+}
+
+export default TopBanner;
