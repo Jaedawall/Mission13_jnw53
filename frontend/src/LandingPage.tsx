e983955d14b276993b9603f135c874bd81cbbf92
Jaewall <jaedawaldram@gmail.com>
2023-04-02 21:47:48 -0600

    Extra Bootstrap

--- frontend/src/LandingPage.tsx
+++ frontend/src/LandingPage.tsx
@@ -5,11 +5,11 @@ function LandingPage() {
     <>
       <Navbar />
       <div className="row"></div>
-      <div>
-        <img src="./JoelHiltonHeadshot.jpg" alt="JoelHeadshot" />
-      </div>
-      <div className="col align-self-center ">
+      <div className="text-center">
         <h1 className={styles.h1}>Welcome to my movie collection website</h1>
+      </div>
+      <div className="text-center">
+        <img src="./JoelHiltonHeadshot.jpg" alt="JoelHeadshot" />
       </div>
     </>
   );
