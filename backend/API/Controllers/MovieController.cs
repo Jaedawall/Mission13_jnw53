ee7c2327829b8ddbc6be14c890176a9e51d9d27b
Jaewall <jaedawaldram@gmail.com>
2023-04-04 15:42:30 -0600

    Backend

---
+++ backend/API/Controllers/MovieController.cs
@@ -0,0 +1,22 @@
+﻿using API.Data;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace API.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class MovieController : Controller
+    {
+        private MovieDbContext context;
+        public MovieController(MovieDbContext temp)
+        {
+            context = temp;
+        }
+        public IEnumerable<MovieCollection> Get()
+        {
+            return context.Movies.ToArray();
+        }
+    }
+}
---
+++ backend/API/Migrations/20230404031635_Initial.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace API.Migrations
+{
+    public partial class Initial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Movies",
+                columns: table => new
+                {
+                    MovieId = table.Column<int>(nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Title = table.Column<string>(nullable: true),
+                    Year = table.Column<int>(nullable: false),
+                    Director = table.Column<string>(nullable: true),
+                    Rating = table.Column<string>(nullable: true),
+                    Category = table.Column<string>(nullable: true),
+                    Edited = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Movies", x => x.MovieId);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Movies");
+        }
+    }
+}
